a4404bab5d0196816705ed04992b8a0df58c175e
Omran_Kabo <73273209+Omrankabo@users.noreply.github.com>
2023-08-28 08:28:52 +0300

    Update dbConfig.ts
    changing mongoose.connect to a string

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connect = async function(){
     
     try{
-        mongoose.connect(process.env.MONGO_URI!)
+        mongoose.connect(`${process.env.MONGO_URI!}`)
     
         const connection = mongoose.connection;
         connection.on('connect',()=>{
-            console.log('mogodb connected successfuly');
+            console.log('MongoDB connected successfully);
             
-        })
+        });
         connection.on('error',()=>{
             console.log('mogodb failed to connect');
             
-        })
+        });
     }catch(error:any){
         console.log(error.message,'Yeah something went wrong');
         
