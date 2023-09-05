7c59d11e0434ca4367d652204319bb6ae4c9c67d
Omran_Kabo <73273209+Omrankabo@users.noreply.github.com>
2023-09-05 21:44:34 +0300

    Update route.ts

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -3,19 +3,17 @@ import connect from '@/dbConfig/dbConfig'
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
 
-// connect to data base 
+//Connect to the database 
 connect()
 
 export async function GET(request:NextRequest) {
     try {
         // get the id 
-        const id = getDataFromToken(request)
-        console.log(id);
+        const id = getDataFromToken(request);
         
         const user = await User.findById(id).select('-password -_v -isAdmin ')
 
         return (NextResponse.json({
-            message: 'You made it and extacuted user informations',
             data: user,
         },{status:200}))
 
